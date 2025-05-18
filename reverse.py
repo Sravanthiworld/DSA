dce69a9239d453e8222ff2f1cf6c30cc5896dc05
Sravanthiworld <sravanthimurukonda.gtl@gmail.com>
2025-05-18 17:38:04 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,7 +1,7 @@
-class Solutions(object):
-  def reverseString(self,s):
-   left,right=0,lens(s)-1
-    while left < right:
-     s[left],s[right]=s[right],s[left]
-     left +=1
-     right-=1
+class Solution(object):
+    def reverseString(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
